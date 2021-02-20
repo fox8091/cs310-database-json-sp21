a7c83c797bb6a7ad00928fc79e1c0e19b5caffda
fox8091 <bfox200012@gmail.com>
2021-02-19 23:13:04 -0600

    Completed 2B

---
+++ src/databasetest/DatabaseTest.java
@@ -0,0 +1,117 @@
+package databasetest;
+
+import java.sql.*;
+import org.json.simple.*;
+
+public class DatabaseTest {
+
+    public JSONArray getJSONData() {
+        
+        JSONArray output = new JSONArray();
+        Connection conn = null;
+        PreparedStatement pstSelect = null, pstUpdate = null;
+        ResultSet resultset = null;
+        ResultSetMetaData metadata = null;
+        
+        String query;
+        
+        boolean hasresults;
+        int resultCount, columnCount = 0;
+        
+        try {
+            
+            /* Identify the Server */
+            
+            String server = ("jdbc:mysql://localhost/p2_test");
+            String username = "root";
+            String password = "CS488";
+            
+            /* Load the MySQL JDBC Driver */
+            
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            
+            /* Open Connection */
+
+            conn = DriverManager.getConnection(server, username, password);
+
+            /* Test Connection */
+            
+            if (conn.isValid(0)) {
+                                
+                /* Prepare Select Query */
+                
+                query = "SELECT * FROM people";
+                pstSelect = conn.prepareStatement(query);
+                
+                /* Execute Select Query */
+                
+                hasresults = pstSelect.execute();                
+                
+                /* Get Results */
+                
+                while ( hasresults || pstSelect.getUpdateCount() != -1 ) {
+
+                    if ( hasresults ) {
+                        
+                        /* Get ResultSet Metadata */
+                        
+                        resultset = pstSelect.getResultSet();
+                        metadata = resultset.getMetaData();
+                        columnCount = metadata.getColumnCount();
+                        
+                        /* Get Data; output to JSON */
+                        
+                        while(resultset.next()) {
+                             JSONObject composeJSON = new JSONObject();
+                             for(int i = 2; i <= columnCount; i++){
+                                 composeJSON.put(metadata.getColumnLabel(i),resultset.getString(i));
+                             }
+                             output.add(composeJSON);
+                        }
+                        
+                    }
+
+                    else {
+
+                        resultCount = pstSelect.getUpdateCount();  
+
+                        if ( resultCount == -1 ) {
+                            break;
+                        }
+
+                    }
+                    
+                    /* Check for More Data */
+
+                    hasresults = pstSelect.getMoreResults();
+
+                }
+                
+            }
+            
+            System.out.println();
+            
+            /* Close Database Connection */
+            
+            conn.close();
+            
+        }
+        
+        catch (Exception e) {}
+        
+        /* Close Other Database Objects */
+        
+        finally {
+            
+            if (resultset != null) { try { resultset.close(); resultset = null; } catch (Exception e) {} }
+            
+            if (pstSelect != null) { try { pstSelect.close(); pstSelect = null; } catch (Exception e) {} }
+            
+            if (pstUpdate != null) { try { pstUpdate.close(); pstUpdate = null; } catch (Exception e) {} }
+            
+        }
+        
+        return output;
+    }
+    
+}
